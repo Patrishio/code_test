95697b488673127063914f55c4dc9713bbe001f3
Patrishio <patrishio@gmail.com>
2019-05-04 20:58:32 +0100

    passed params to service sends requests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,16 @@ class StaticPagesController < ApplicationController
   end
 
   def submit
-
+    SendsRequest.new(
+      params[:name],
+      params[:business_name],
+      params[:telephone_number],
+      params[:email]
+    )
+    
+    respond_to do |format|
+      format.html { render :home }
+    end
+    
   end
 end
--- app/services/sends_request.rb
+++ app/services/sends_request.rb
@@ -1,3 +1,13 @@
 class SendsRequest
-  
+  def initialize(
+    name,
+    business_name,
+    telephone_number,
+    email
+  )
+    @name = name
+    @business_name = business_name
+    @telephone_number = telephone_number
+    @email = email
+  end
 end
